351a7f2585cca2523e6bfdc1a8499c3f477c7fa4
Ababington <74577783+Ababington@users.noreply.github.com>
2020-11-16 21:29:41 -0500

    Add files via upload

---
+++ csharp-capstone-module-2-team-3/TenmoServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using TenmoServer.DAO;
+
+namespace TenmoServer
+{
+    public class Program
+    {
+       
+        public static void Main(string[] args)
+        {
+            IUserDAO userDAO = new UserSqlDAO(@"Data Source =.\SQLEXPRESS;Initial Catalog = tenmo; Integrated Security = True");
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
